25c2dd1d978c7caadf007bba325d6ce86490e28a
Siddhi1432 <siddhirmane14@gmail.com>
2026-05-30 22:15:40 +0530

    Task 4: Learned React useState and built counter

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,23 +1,28 @@
-import Welcome from "./components/Welcome"
-import Profile from "./components/Profile"
-import Goal from "./components/Goal"
+import Welcome from "./components/Welcome";
+import Profile from "./components/Profile";
+import Goal from "./components/Goal";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <h1>React Props Demo</h1>
+      <h1>React State Demo </h1>
 
       <Welcome name="Siddhi" />
-      <Welcome name="React Learner" />
 
       <Profile
         name="Siddhi Mane"
         role="Future Full Stack Developer"
       />
 
-      <Goal company = "Google" package = "20 LPA"/>
+      <Goal
+        company="ThoughtWorks"
+        package="12 LPA"
+      />
+
+      <Counter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
